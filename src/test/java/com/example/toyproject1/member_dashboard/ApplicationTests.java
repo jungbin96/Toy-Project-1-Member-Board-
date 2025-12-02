9fb1f082d8f700f710053cb9470e3378f126c832
jungbin96 <xg961215@gmail.com>
2025-12-02 21:44:55 +0900

    init backend main branch

---
+++ src/test/java/com/example/toyproject1/member_dashboard/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyproject1.member_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
