e259bb4e1851eab7bb4fd70413c80a20d92e5b44
jungbin96 <xg961215@gmail.com>
2025-12-08 21:44:48 +0900

    Add init sample datas in members table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO members (name, email, status, created_at, updated_at)
+VALUES ('홍길동', 'hong@test.com', 'ACTIVE', NOW(), NOW());
+
+INSERT IGNORE INTO members (name, email, status, created_at, updated_at)
+VALUES ('김철수', 'kim@test.com', 'ACTIVE', NOW(), NOW());
