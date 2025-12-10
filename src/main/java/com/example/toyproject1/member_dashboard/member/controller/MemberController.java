917907caacf7751cd788f3d3744edd38bbb3176f
jungbin <xg961215@gmail.com>
2025-12-10 16:47:12 +0900

    feat : add api controller (POST createMember)

---
+++ src/main/java/com/example/toyproject1/member_dashboard/member/controller/MemberController.java
@@ -0,0 +1,29 @@
+package com.example.toyproject1.member_dashboard.member.controller;
+
+import com.example.toyproject1.member_dashboard.member.dto.MemberCreateRequest;
+import com.example.toyproject1.member_dashboard.member.dto.MemberResponse;
+import com.example.toyproject1.member_dashboard.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    //회원 생성
+    @PostMapping
+    public ResponseEntity<MemberResponse> createMember(@RequestBody MemberCreateRequest request){
+        MemberResponse response = memberService.createMember(request);
+        return ResponseEntity
+                .created(URI.create("/api/members/"+response.getId()))
+                .body(response);
+    }
+
+
+}
