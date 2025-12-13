90d4d72a45da7859377457f67c2b353dfcd37625
jungbin96 <xg961215@gmail.com>
2025-12-13 21:10:56 +0900

    test: add MemberController WebMvcTest skeleton

---
+++ src/main/java/com/example/toyproject1/member_dashboard/member/controller/MemberControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.toyproject1.member_dashboard.member.controller;
+
+import com.example.toyproject1.member_dashboard.member.repository.MemberRepository;
+import com.example.toyproject1.member_dashboard.member.service.MemberService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MemberController.class)
+public class MemberControllerTest {
+
+    /*[ MockMvc ]
+            ↓ (가짜 HTTP 요청)
+            [ MemberController ]   ← 실제
+            ↓
+            [ MemberService ]      ← Mockito 가짜
+            */
+
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    MemberService memberService;
+}
